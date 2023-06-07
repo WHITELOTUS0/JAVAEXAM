759c8cc4b683612b80d1b837ebf4b5389b317c21
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-07 17:21:26 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,5 +1,11 @@
 public class Person implements Cloneable{
     private String name;
 
-    
+    Person(String name){
+        this.name=name;
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
