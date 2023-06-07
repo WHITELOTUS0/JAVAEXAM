3862fc81387d8cc56a287b6ca0c2fa5be6db791e
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-07 20:08:58 +0300

    Update Pupil.java

--- Pupil.java
+++ Pupil.java
@@ -1,7 +1,8 @@
 //Superclass is School
 public class Pupil extends School{
-    School(){
+    Pupil(){
         super("Kampala");
+        System.out.println("From sub class constructor");
     }
     public static void main(String[] args) {
         new Pupil();
