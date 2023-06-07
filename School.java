eed1abff6cdb5b3bcd055277d464b48d542684cf
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-07 20:03:49 +0300

    Update School.java

--- School.java
+++ School.java
@@ -1,9 +1,10 @@
 //Subclass is Pupil
 public class School {
     School(){
-        System.out.println("School Constructor");
+        System.out.println("I go to School");
     }
     School (String location){
         System.out.println(location);
     }
+    
 }
