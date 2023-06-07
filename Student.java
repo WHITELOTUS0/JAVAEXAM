58b5eb3a44557f54f369816a591d02ff65ff710d
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-07 18:55:04 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-public class Student {
+public class Student implements Cloneable{
     public String name;
     public int age;
 
@@ -9,9 +9,23 @@ public class Student {
 
     @Override
     public Object clone() throws CloneNotSupportedException{
-        Student clone = (Student) super.clone();
-        clone.name=this.name;
-        clone.age=this.age;
-        return clone;
+        Student cloned = (Student) super.clone();
+        cloned.name=this.name;
+        cloned.age=this.age;
+        return cloned;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Student obj = new Student("Glorry", 20);
+
+        Student obj2 = (Student) obj.clone();
+        System.out.println(obj.name);
+        System.out.println(obj2.name);
+
+        obj.name = "John";
+        obj.age = 30;
+
+        System.out.println(obj.name);
+        System.out.println(obj2.name);
     }
 }
