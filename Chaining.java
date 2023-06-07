07817753b4ce4ecff361071f78b08fb7cf2580d7
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-07 19:30:26 +0300

    Update Chaining.java

--- Chaining.java
+++ Chaining.java
@@ -1,3 +1,11 @@
 public class Chaining {
-    
+    Chaining(){
+        this(5);
+        System.out.println("No arg constructor");
+    }
+
+    Chaining(int x){
+        this("Kampala", 20);
+        System.out.println("One Arg Constructor : " +x);
+    }
 }
