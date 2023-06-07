e15fb1cb34509a15acd1cc237d86788ae962b374
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-07 18:40:47 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,4 +1,14 @@
 public class Student {
     public String name;
     public int age;
+
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+        
+    }
 }
