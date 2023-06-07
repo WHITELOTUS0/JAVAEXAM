4bf967dd2b575526af630200fc2de3ce15c62b9f
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-07 20:00:59 +0300

    Update School.java

--- School.java
+++ School.java
@@ -1,6 +1,8 @@
 public class School {
-    School(){
-        System.out.println("School Constructor");
+    // School(){
+    //     System.out.println("School Constructor");
+    // }
+    static {
+        System.out.println("Shariff");
     }
 }
-
