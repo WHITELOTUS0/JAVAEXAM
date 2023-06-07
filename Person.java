bd2cb639ff548d02ecff6079aaccf242e5586ac8
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-07 17:38:24 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -5,23 +5,13 @@ public class Person implements Cloneable {
         this.name = name;
     }
     
-    @Override
-    public Object clone() throws CloneNotSupportedException{
-        return super.clone();
-    }
+   
     public static void main(String[] args) throws Exception {
         Person obj = new Person("Glorry");
         
         Person obj2 = (Person) obj.clone();
         System.out.println(obj.name);
         System.out.println(obj2.name);
-        
-        obj.name = "Shariff";
-        obj2 = (Person)obj.clone();
-        
 
-        
-        System.out.println(obj.name);
-        System.out.println(obj2.name);
     }
 }
