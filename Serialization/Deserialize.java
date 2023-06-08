fa0744ef10071f9b4335aeb486f1b7269078fc7c
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-08 18:19:41 +0300

    Update Deserialize.java

--- Serialization/Deserialize.java
+++ Serialization/Deserialize.java
@@ -3,5 +3,9 @@ package Serialization;
 import java.io.*;
 
 public class Deserialize implements Serializable{
-    
+    try {
+        
+    } catch (IOException e | ClassNot) {
+        // TODO: handle exception
+    }
 }
