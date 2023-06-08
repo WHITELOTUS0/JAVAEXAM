10221a9cac0bf9a30911899aec84a21fd1a05c42
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-08 17:26:29 +0300

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,3 +1,10 @@
-public class Dog {
-    
+public class Dog extends Animal{
+    public void bark(){
+        System.out.println("Dog barks");
+    }
+
+    public static void main(String[] args) {
+        Animal obj = new Dog();
+        obj.eat();
+    }
 }
