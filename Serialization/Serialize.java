9f9592d3c1c3bb72c20b26d678e552670b5a61e7
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-08 17:58:23 +0300

    Update Serialize.java

--- Serialization/Serialize.java
+++ Serialization/Serialize.java
@@ -1,5 +1,7 @@
 package Serialization;
 
+import java.io.IOException;
+
 public class Serialize {
     public String name;
     public int age;
@@ -7,5 +9,13 @@ public class Serialize {
     Serialize(String name, int age){
         this.name=name;
         this.age=age;
+    }
+
+    public static void main(String[] args) {
+        try{
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
