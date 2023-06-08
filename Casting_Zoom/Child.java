1bc749834d8f89b7591a6fb49aeab34b6cf7f23d
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-08 20:34:33 +0300

    casting

--- Casting_Zoom/Child.java
+++ Casting_Zoom/Child.java
@@ -1,5 +1,15 @@
 package Casting_Zoom;
 
-public class Child {
-    
+public class Child extends Parent{
+    public void child(){
+        System.out.println("From Child class");
+    }
+
+    public static void main(String[] args) {
+        Parent obj = new Child();//Upcasting
+        obj.parent();
+
+        Child obj2 = (Child)obj;//Downcasting
+        obj2.child();
+    }
 }
--- Casting_Zoom/Parent.java
+++ Casting_Zoom/Parent.java
@@ -2,6 +2,6 @@ package Casting_Zoom;
 
 public class Parent {
     public void parent(){
-        System.out.println("Parent");
+        System.out.println("From Parent Class");
     }
 }
