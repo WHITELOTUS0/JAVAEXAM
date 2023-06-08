3576bc2b218049329b4253cb375e9145d078267a
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-08 18:28:34 +0300

    Update Deserialize.java

--- Serialization/Deserialize.java
+++ Serialization/Deserialize.java
@@ -3,14 +3,23 @@ package Serialization;
 import java.io.*;
 
 public class Deserialize {
-    try {
-        //Deserialization
-        FileInputStream fileIn = new FileInputStream("se-file.ser");
-        ObjectInputStream in = new ObjectInputStream(fileIn);
-        //Read the object
+    public static void main(String[] args) {
+        try {
+            // Deserialization
+            FileInputStream fileIn = new FileInputStream("se-file.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            // Read the object
+            Serialize deserializedObj = (Serialize) in.readObject();
+            in.close();
+            fileIn.close();
+            System.out.println("Object deserialized successfully.");
 
+            System.out.println("Name: " + deserializedObj.name);
+            System.out.println("Age: " + deserializedObj.age);
 
-    } catch (IOException | ClassNotFoundException e) {
-        e.printStackTrace();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
 }
