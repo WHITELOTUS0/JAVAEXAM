ce0ce3c2ce9a0317a975c66f626d30537ba0a2c4
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-08 18:25:37 +0300

    Update Deserialize.java

--- Serialization/Deserialize.java
+++ Serialization/Deserialize.java
@@ -2,9 +2,14 @@ package Serialization;
 
 import java.io.*;
 
-public class Deserialize implements Serializable{
+public class Deserialize {
     try {
-        
+        //Deserialization
+        FileInputStream fileIn = new FileInputStream("se-file.ser");
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        //Read the object
+
+
     } catch (IOException | ClassNotFoundException e) {
         e.printStackTrace();
     }
