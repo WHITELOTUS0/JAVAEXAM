8e5ba9760890bb6c6b3613aa6ea11504cb7e62a2
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-08 20:27:06 +0300

    Update Interning.java

--- Interning/Interning.java
+++ Interning/Interning.java
@@ -8,5 +8,9 @@ public class Interning {
         System.out.println(s==s1);
         String s2 = new String("Kato").intern();
         System.out.println(s1==s2);
+
+        String m = new String("Kampala");
+        String n = new String("Kampala");
+        System.out.println(m==n);
     }
 }
