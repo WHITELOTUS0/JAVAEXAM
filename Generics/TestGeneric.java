ec7ffa6edc9e2d53b6116a1bb31333a7b9550ea2
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-09 20:59:27 +0300

    Update TestGeneric.java

--- Generics/TestGeneric.java
+++ Generics/TestGeneric.java
@@ -1,7 +1,11 @@
 package Generics;
 
+import java.util.*;
+
 public class TestGeneric {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>();
+        names.add("Glorry");
+        names.add("Sibo");
     }
 }
