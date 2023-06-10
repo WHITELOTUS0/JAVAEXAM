b7055862195c0ec06d5d686ce2b6de87428f12b0
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-10 20:31:32 +0300

    Update JDBCCon.java

--- JDBCConnection/JDBCCon.java
+++ JDBCConnection/JDBCCon.java
@@ -2,23 +2,38 @@ package JDBCConnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class JDBCCon {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/mydatabase";
-        String username = "root";
-        String password = "Bujumbura1#";
+        String username = "";
+        String password = "";
 
         try {
             // Register the JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
             // Establish a connection
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the database!");
 
             // Perform database operations...
+            String insertQuery = "INSERT INTO mytable (Name, Password) VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(insertQuery);
+            // Set the parameter values
+            statement.setString(1, "Glorry");
+            statement.setString(2, "password123");
+
+            // Execute the INSERT statement
+            int rowsAffected = statement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Values inserted successfully!");
+            } else {
+                System.out.println("No rows affected. Insertion failed.");
+            }
 
             // Close the connection
             connection.close();
