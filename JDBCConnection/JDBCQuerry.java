4955f4728c7c6c1109a6a2c8df7789ace4c66ac4
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-13 22:55:05 +0300

    Update JDBCQuerry.java

--- JDBCConnection/JDBCQuerry.java
+++ JDBCConnection/JDBCQuerry.java
@@ -4,13 +4,19 @@ import java.sql.*;
 public class JDBCQuerry {
     public static void main(String[] args) {
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            String url = "jsbc:mysql://localhost:3306/mydatabase";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/mydatabase";
             String user = "root";
             String password = "Bujumbura1#";
 
             Connection con = DriverManager.getConnection(url, user, password);
             Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("Select * from mytable");
+
+            while(rs.next()){
+                System.out.println(rs.getString(1));
+                System.out.println(rs.getString("password"));
+            }
 
         }catch(SQLException | ClassNotFoundException e){
             e.printStackTrace();
