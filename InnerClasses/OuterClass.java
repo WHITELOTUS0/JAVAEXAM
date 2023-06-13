24d2b94b26aea6524f0468066d7f97db523d2e5a
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-13 22:03:32 +0300

    Create OuterClass.java

---
+++ InnerClasses/OuterClass.java
@@ -0,0 +1,15 @@
+package InnerClasses;
+
+public class OuterClass {
+    public int age=10;
+
+    public class InnerClass{
+        public void printAge(){
+            System.out.println("Age is "+age);
+            }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
