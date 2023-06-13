4d350ce3f736b8c565ef9d9b77aefa682455c556
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-13 22:42:58 +0300

    Update JDBCQuerry.java

--- JDBCConnection/JDBCQuerry.java
+++ JDBCConnection/JDBCQuerry.java
@@ -1,7 +1,8 @@
 package JDBCConnection;
+import java.sql.*;
 
 public class JDBCQuerry {
     public static void main(String[] args) {
-        
+        Class.forName("com.mysql.cj.jdbc.Driver");
     }
 }
